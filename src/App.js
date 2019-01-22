5d317d3bf6e706058fe0119d180d3e7e81c7c90b
unknown <alfred.osorio@outlook.com>
2019-01-23 02:57:45 +1100

    Fix browser-compatibility issues

--- src/App.js
+++ src/App.js
@@ -9,30 +9,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <main className="App-header">
+        <main className="App-main">
         {/* Audio Control */}
           <div className="Audio-player">
             <AudioPlayer file={human_dragon}/>
           </div>
         {/* Logo */}
           <div className="Logo-header">
-            <img src={logo} alt='og_nerd logo'></img>
+            <img src={logo} alt='og_nerd logo' />
           </div>
         {/* Coming Soon */}
           <div className="Coming-soon">
             <p>COMING SOON</p>
           </div>
-        {/* Footer */}
-          <div className="Footer">
-          {/* Social Media Buttons */}
-            <div className="Social-media-buttons">
-              <i className="fab fa-facebook-f"></i>
-              <i className="fab fa-instagram"></i>
-              <i className="fab fa-youtube"></i>
-            </div>
-            <p>&copy; OG Nerd 2019</p>
-          </div>
         </main>
+        {/* Footer */}
+        <div className="Footer">
+        {/* Social Media Buttons */}
+          <div className="Social-media-buttons">
+            <i className="fab fa-facebook-f"></i>
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-youtube"></i>
+          </div>
+          <p>&copy; OG Nerd 2019</p>
+        </div>
       </div>
     );
   }
